c9136901e830cd256668945ed7b27625744256b5
Abhinav Aggarwal <92445847+agg-geek@users.noreply.github.com>
2024-01-07 22:04:38 +0530

    Add issue desc and files related to publisher subscriber pattern

--- src/js/controller.js
+++ src/js/controller.js
@@ -28,4 +28,11 @@ const controlRecipe = async function () {
 
 controlRecipe();
 
+// we are listening for events here (should belong to recipeView)
+// but this requires controlRecipe fn which is present in the controller only
+// we may think we can import controlRecipe in the view
+// this is bad, as the view and model should not be aware of controller
+// and only the controller should delegate tasks to the model and view
+// this is a standard problem, and we use design patterns to solve them
+// and use publisher-subscriber pattern  (the design pattern)
 ['hashchange', 'load'].forEach(evt => window.addEventListener(evt, controlRecipe));
