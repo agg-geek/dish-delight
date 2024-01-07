260f94bdb970c9e7a2e21afda8a95043e6b9624c
Abhinav Aggarwal <92445847+agg-geek@users.noreply.github.com>
2024-01-07 21:26:25 +0530

    Refactor ingredient markup generator

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -76,21 +76,7 @@ class RecipeView {
         <div class="recipe__ingredients">
             <h2 class="heading--2">Recipe ingredients</h2>
             <ul class="recipe__ingredient-list">
-                ${this.#recipeData.ingredients
-					.map(
-						ing =>
-							`<li class="recipe__ingredient">
-                    <svg class="recipe__icon">
-                        <use href="${icons}#icon-check"></use>
-                    </svg>
-                    <div class="recipe__quantity">${ing.quantity ? new Fraction(ing.quantity).toString() : ''}</div>
-                    <div class="recipe__description">
-                        <span class="recipe__unit">${ing.unit}</span>
-                        ${ing.description}
-                    </div>
-                </li>`
-					)
-					.join('')}
+                ${this.#recipeData.ingredients.map(ing => this.#generateIngredientMarkup(ing)).join('')}
             </ul>
         </div>
 
@@ -114,6 +100,19 @@ class RecipeView {
                 </svg>
             </a>
         </div>`;
+	}
+
+	#generateIngredientMarkup(ing) {
+		return `<li class="recipe__ingredient">
+                    <svg class="recipe__icon">
+                        <use href="${icons}#icon-check"></use>
+                    </svg>
+                    <div class="recipe__quantity">${ing.quantity ? new Fraction(ing.quantity).toString() : ''}</div>
+                    <div class="recipe__description">
+                        <span class="recipe__unit">${ing.unit}</span>
+                        ${ing.description}
+                    </div>
+                </li>`;
 	}
 }
 
