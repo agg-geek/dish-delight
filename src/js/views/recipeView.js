8b8854a0a0fa7637b29bc65c43193ff180602498
Abhinav Aggarwal <92445847+agg-geek@users.noreply.github.com>
2024-01-07 21:23:29 +0530

    Fix displaying null quantity

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -83,7 +83,7 @@ class RecipeView {
                     <svg class="recipe__icon">
                         <use href="${icons}#icon-check"></use>
                     </svg>
-                    <div class="recipe__quantity">${new Fraction(ing.quantity).toString()}</div>
+                    <div class="recipe__quantity">${ing.quantity ? new Fraction(ing.quantity).toString() : ''}</div>
                     <div class="recipe__description">
                         <span class="recipe__unit">${ing.unit}</span>
                         ${ing.description}
