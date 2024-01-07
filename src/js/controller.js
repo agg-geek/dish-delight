f60804d2cfa2ff906c7b1bd8ff8d13f6b9095b44
Abhinav Aggarwal <92445847+agg-geek@users.noreply.github.com>
2024-01-07 18:47:49 +0530

    Rename recipe properties

--- src/js/controller.js
+++ src/js/controller.js
@@ -13,15 +13,32 @@ const timeout = function (s) {
 
 const showRecipe = async function () {
 	try {
-		// const res = await fetch('https://forkify-api.herokuapp.com/api/v2/recipes?search=pizza');
-		const res = await fetch('https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886');
-		// const res = await fetch('https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886zzzz');
+		// const res = await fetch('https://forkify-api.herokuapp.com/api/v2/recipes?search=pizza'); // all recipes
+
+		const res = await fetch('https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'); // pizza
+		// const res = await fetch('https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604691c37cdc054bd0b8'); // chocolate
+
+		// const res = await fetch('https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886zzzz'); // bad id
 		const data = await res.json();
 		console.log(data);
 
 		if (!res.ok) throw new Error(`${data.message} (${res.status})`);
 
-		const { recipe } = data.data;
+		let { recipe } = data.data;
+		console.log(recipe);
+
+		recipe = {
+			id: recipe.id,
+			title: recipe.title,
+			servings: recipe.servings,
+			publisher: recipe.publisher,
+			ingredients: recipe.ingredients,
+			// remove underscores
+			cookingTime: recipe.cooking_time,
+			sourceUrl: recipe.source_url,
+			imageUrl: recipe.image_url,
+		};
+
 		console.log(recipe);
 	} catch (err) {
 		console.log(err);
