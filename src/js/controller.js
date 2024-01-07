ae9f7430fc4b76b79697a2ba65b7bc3b1122fca4
Abhinav Aggarwal <92445847+agg-geek@users.noreply.github.com>
2024-01-07 22:57:29 +0530

    Implement searchView design pattern

--- src/js/controller.js
+++ src/js/controller.js
@@ -24,11 +24,12 @@ const controlSearchResults = async function () {
 	const query = searchView.getQuery();
 	if (!query) return;
 
-	await model.loadSearchResults('pizza');
+	await model.loadSearchResults(query);
 	console.log(model.state.search.results);
 };
 
 const init = function () {
+	searchView.addHandlerSearch(controlSearchResults);
 	recipeView.addHandlerRender(controlRecipe);
 };
 
--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -4,6 +4,13 @@ class SearchView {
 	getQuery() {
 		return this.#parentElem.querySelector('.search__field').value;
 	}
+
+	addHandlerSearch(handler) {
+		this.#parentElem.addEventListener('submit', function (evt) {
+			evt.preventDefault(); // since parentElem is a form
+			handler();
+		});
+	}
 }
 
 export default new SearchView();
