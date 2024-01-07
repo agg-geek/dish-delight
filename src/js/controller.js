8784a436ee21cfde6c10dc52c7da0b6fac84af93
Abhinav Aggarwal <92445847+agg-geek@users.noreply.github.com>
2024-01-08 00:00:38 +0530

    Implement resultsView

--- src/js/controller.js
+++ src/js/controller.js
@@ -3,6 +3,7 @@ import 'regenerator-runtime/runtime';
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
+import resultsView from './views/resultsView.js';
 
 const controlRecipe = async function () {
 	try {
@@ -25,7 +26,8 @@ const controlSearchResults = async function () {
 	if (!query) return;
 
 	await model.loadSearchResults(query);
-	console.log(model.state.search.results);
+	// console.log(model.state.search.results);
+	resultsView.render(model.state.search.results);
 };
 
 const init = function () {
---
+++ src/js/views/resultsView.js
@@ -0,0 +1,34 @@
+import icons from 'url:../../img/icons.svg';
+import View from './View.js';
+
+class ResultsView extends View {
+	_parentElem = document.querySelector('.results');
+
+	_generateMarkup() {
+		// we call resultsView.render(<searchResultsData>) in controller
+		// .render (inherited from parent class View) then assigns that data
+		//  to _data (also inherited) and then we simply render that data
+		// things are called preview simply because of the classname
+		console.log(this._data);
+		return this._data.map(preview => this._generateMarkupPreview(preview)).join('');
+	}
+
+	_generateMarkupPreview(preview) {
+		return `<li class="preview">
+            <a class="preview__link preview__link--active" href="#${preview.id}">
+                <figure class="preview__fig">
+                    <img src="${preview.imageUrl}" alt="${preview.title}" />
+                </figure>
+                <div class="preview__data">
+                    <h4 class="preview__title">${preview.title}</h4>
+                    <p class="preview__publisher">${preview.publisher}</p>
+                    <div class="preview__user-generated">
+                        <svg><use href="${icons}#icon-user"></use></svg>
+                    </div>
+                </div>
+            </a>
+        </li>`;
+	}
+}
+
+export default new ResultsView();
