a307c1707ed04c3a51d0a6a9e2ab8675ff9dd664
Abhinav Aggarwal <92445847+agg-geek@users.noreply.github.com>
2024-01-07 21:53:11 +0530

    Define constant for timeout in config file

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -1,3 +1,5 @@
+import { TIMEOUT_SEC } from './config';
+
 const timeout = function (s) {
 	return new Promise(function (_, reject) {
 		setTimeout(function () {
@@ -8,7 +10,7 @@ const timeout = function (s) {
 
 export const newRequest = async function (url) {
 	try {
-		const res = await Promise.race([fetch(url), timeout(0.5)]);
+		const res = await Promise.race([fetch(url), timeout(TIMEOUT_SEC)]);
 		const data = await res.json();
 
 		if (!res.ok) throw new Error(`${data.message} (${res.status})`);
