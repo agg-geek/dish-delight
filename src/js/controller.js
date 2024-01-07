0260b4a23d1174d9ed641f5700bb7a7681139930
Abhinav Aggarwal <92445847+agg-geek@users.noreply.github.com>
2024-01-07 19:00:38 +0530

    Display recipe ingredients

--- src/js/controller.js
+++ src/js/controller.js
@@ -99,27 +99,17 @@ const showRecipe = async function () {
         <div class="recipe__ingredients">
             <h2 class="heading--2">Recipe ingredients</h2>
             <ul class="recipe__ingredient-list">
-                <li class="recipe__ingredient">
+                ${recipe.ingredients.map(ing => 
+                `<li class="recipe__ingredient">
                     <svg class="recipe__icon">
                         <use href="src/img/icons.svg#icon-check"></use>
                     </svg>
-                    <div class="recipe__quantity">1000</div>
+                    <div class="recipe__quantity">${ing.quantity}</div>
                     <div class="recipe__description">
-                        <span class="recipe__unit">g</span>
-                        pasta
+                        <span class="recipe__unit">${ing.unit}</span>
+                        ${ing.description}
                     </div>
-                </li>
-
-                <li class="recipe__ingredient">
-                    <svg class="recipe__icon">
-                        <use href="src/img/icons.svg#icon-check"></use>
-                    </svg>
-                    <div class="recipe__quantity">0.5</div>
-                    <div class="recipe__description">
-                        <span class="recipe__unit">cup</span>
-                        ricotta cheese
-                    </div>
-                </li>
+                </li>`)}
             </ul>
         </div>
 
