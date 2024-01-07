cb11e795fa7fe2a00316b11f2412b3a3c8ee3d74
Abhinav Aggarwal <92445847+agg-geek@users.noreply.github.com>
2024-01-08 00:01:59 +0530

    Fix search result UI

--- src/js/views/resultsView.js
+++ src/js/views/resultsView.js
@@ -15,16 +15,18 @@ class ResultsView extends View {
 
 	_generateMarkupPreview(preview) {
 		return `<li class="preview">
-            <a class="preview__link preview__link--active" href="#${preview.id}">
+            <a class="preview__link" href="#${preview.id}">
                 <figure class="preview__fig">
                     <img src="${preview.imageUrl}" alt="${preview.title}" />
                 </figure>
                 <div class="preview__data">
                     <h4 class="preview__title">${preview.title}</h4>
                     <p class="preview__publisher">${preview.publisher}</p>
-                    <div class="preview__user-generated">
-                        <svg><use href="${icons}#icon-user"></use></svg>
-                    </div>
+                    <!-- 
+                        <div class="preview__user-generated">
+                            <svg><use href="${icons}#icon-user"></use></svg>
+                        </div>
+                    -->
                 </div>
             </a>
         </li>`;
