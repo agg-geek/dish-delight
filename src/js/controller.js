89e86f2882eee29d99bfd5eee507e84bff825520
Abhinav Aggarwal <92445847+agg-geek@users.noreply.github.com>
2024-01-07 18:35:09 +0530

    Load data from API and perform basic error handling

--- src/js/controller.js
+++ src/js/controller.js
@@ -10,3 +10,22 @@ const timeout = function (s) {
 		}, s * 1000);
 	});
 };
+
+const showRecipe = async function () {
+	try {
+		// const res = await fetch('https://forkify-api.herokuapp.com/api/v2/recipes?search=pizza');
+		const res = await fetch('https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886');
+		// const res = await fetch('https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886zzzz');
+		const data = await res.json();
+		console.log(data);
+
+		if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+
+		const { recipe } = data.data;
+		console.log(recipe);
+	} catch (err) {
+		console.log(err);
+	}
+};
+
+showRecipe();
