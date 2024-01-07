3a8b6843707c8e4f0579abace29f21d8b9e7aacc
Abhinav Aggarwal <92445847+agg-geek@users.noreply.github.com>
2024-01-07 21:22:16 +0530

    Display fractions instead of floats

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,3 +1,4 @@
+import { Fraction } from 'fractional';
 import icons from 'url:../../img/icons.svg';
 
 class RecipeView {
@@ -82,7 +83,7 @@ class RecipeView {
                     <svg class="recipe__icon">
                         <use href="${icons}#icon-check"></use>
                     </svg>
-                    <div class="recipe__quantity">${ing.quantity}</div>
+                    <div class="recipe__quantity">${new Fraction(ing.quantity).toString()}</div>
                     <div class="recipe__description">
                         <span class="recipe__unit">${ing.unit}</span>
                         ${ing.description}
