1d8fd6a3c29e43b3815875cda13cde62611324bc
Abhinav Aggarwal <92445847+agg-geek@users.noreply.github.com>
2024-01-07 21:31:43 +0530

    Create config file

--- src/js/model.js
+++ src/js/model.js
@@ -1,15 +1,13 @@
+import 'regenerator-runtime/runtime';
+import { API_URL } from './config';
+
 export const state = {
 	recipe: {},
 };
 
 export const loadRecipe = async function (recipeId) {
 	try {
-		// const res = await fetch('https://forkify-api.herokuapp.com/api/v2/recipes?search=pizza'); // all recipes
-		// const res = await fetch('https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'); // pizza
-		// const res = await fetch('https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604691c37cdc054bd0b8'); // chocolate
-		// const res = await fetch('https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886zzzz'); // bad id
-
-		const res = await fetch(`https://forkify-api.herokuapp.com/api/v2/recipes/${recipeId}`);
+		const res = await fetch(`${API_URL}/${recipeId}`);
 		const data = await res.json();
 
 		if (!res.ok) throw new Error(`${data.message} (${res.status})`);
