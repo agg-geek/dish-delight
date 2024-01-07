766f34a55e0d95306ecfff35e3ab987192f575bb
Abhinav Aggarwal <92445847+agg-geek@users.noreply.github.com>
2024-01-07 18:55:16 +0530

    Display recipe

--- src/js/controller.js
+++ src/js/controller.js
@@ -13,6 +13,8 @@ const timeout = function (s) {
 
 const showRecipe = async function () {
 	try {
+		// Loading recipe
+
 		// const res = await fetch('https://forkify-api.herokuapp.com/api/v2/recipes?search=pizza'); // all recipes
 
 		const res = await fetch('https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'); // pizza
@@ -20,12 +22,12 @@ const showRecipe = async function () {
 
 		// const res = await fetch('https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886zzzz'); // bad id
 		const data = await res.json();
-		console.log(data);
+		// console.log(data);
 
 		if (!res.ok) throw new Error(`${data.message} (${res.status})`);
 
 		let { recipe } = data.data;
-		console.log(recipe);
+		// console.log(recipe);
 
 		recipe = {
 			id: recipe.id,
@@ -40,6 +42,107 @@ const showRecipe = async function () {
 		};
 
 		console.log(recipe);
+
+		// =====================================================================
+		// Render recipe
+
+		// prettier-ignore
+		const html = 
+        `<figure class="recipe__fig">
+            <img src="${recipe.imageUrl}" alt="${recipe.title}" class="recipe__img" />
+            <h1 class="recipe__title">
+                <span>${recipe.title}</span>
+            </h1>
+        </figure>
+
+        <div class="recipe__details">
+            <div class="recipe__info">
+                <svg class="recipe__info-icon">
+                    <use href="src/img/icons.svg#icon-clock"></use>
+                </svg>
+                <span class="recipe__info-data recipe__info-data--minutes">${recipe.cookingTime}</span>
+                <span class="recipe__info-text">minutes</span>
+            </div>
+            <div class="recipe__info">
+                <svg class="recipe__info-icon">
+                    <use href="src/img/icons.svg#icon-users"></use>
+                </svg>
+                <span class="recipe__info-data recipe__info-data--people">${recipe.servings}</span>
+                <span class="recipe__info-text">servings</span>
+
+                <div class="recipe__info-buttons">
+                    <button class="btn--tiny btn--increase-servings">
+                        <svg>
+                            <use href="src/img/icons.svg#icon-minus-circle"></use>
+                        </svg>
+                    </button>
+                    <button class="btn--tiny btn--increase-servings">
+                        <svg>
+                            <use href="src/img/icons.svg#icon-plus-circle"></use>
+                        </svg>
+                    </button>
+                </div>
+            </div>
+
+            <div class="recipe__user-generated">
+                <svg>
+                    <use href="src/img/icons.svg#icon-user"></use>
+                </svg>
+            </div>
+            <button class="btn--round">
+                <svg class="">
+                    <use href="src/img/icons.svg#icon-bookmark-fill"></use>
+                </svg>
+            </button>
+        </div>
+
+        <div class="recipe__ingredients">
+            <h2 class="heading--2">Recipe ingredients</h2>
+            <ul class="recipe__ingredient-list">
+                <li class="recipe__ingredient">
+                    <svg class="recipe__icon">
+                        <use href="src/img/icons.svg#icon-check"></use>
+                    </svg>
+                    <div class="recipe__quantity">1000</div>
+                    <div class="recipe__description">
+                        <span class="recipe__unit">g</span>
+                        pasta
+                    </div>
+                </li>
+
+                <li class="recipe__ingredient">
+                    <svg class="recipe__icon">
+                        <use href="src/img/icons.svg#icon-check"></use>
+                    </svg>
+                    <div class="recipe__quantity">0.5</div>
+                    <div class="recipe__description">
+                        <span class="recipe__unit">cup</span>
+                        ricotta cheese
+                    </div>
+                </li>
+            </ul>
+        </div>
+
+        <div class="recipe__directions">
+            <h2 class="heading--2">How to cook it</h2>
+            <p class="recipe__directions-text">
+                This recipe was carefully designed and tested by
+                <span class="recipe__publisher">${recipe.publisher}</span>. Please check out directions at their
+                website.
+            </p>
+            <a
+                class="btn--small recipe__btn"
+                href="${recipe.sourceUrl}"
+                target="_blank"
+            >
+                <span>Directions</span>
+                <svg class="search__icon">
+                    <use href="src/img/icons.svg#icon-arrow-right"></use>
+                </svg>
+            </a>
+        </div>`
+
+		recipeContainer.insertAdjacentHTML('afterbegin', html);
 	} catch (err) {
 		console.log(err);
 	}
